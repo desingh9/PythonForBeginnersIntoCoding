29c0c921395298fa47cc6b9aa2536fce766b649d
devendaSingh <desingh319@gmail.com>
2020-08-01 16:45:44 +0530

    esle comment update Q1

--- Day6_Only_Answers/if_else_Q1.py
+++ Day6_Only_Answers/if_else_Q1.py
@@ -1,11 +1,9 @@
 #1.) Write a program to check whether a entered character is lowercase ( a to z ) or uppercase ( A to Z ).
 
-chr=(input("Enter Charactor:"))
-#A=chr(65,90)
-#for i in range(65,90)
-if (ord(chr) >=65 and ord(chr)<=90):
+chr=(input("Enter Charactor:")) #chr for ASCII coding output 
+if (ord(chr) >=65 and ord(chr)<=90): #ord to give output in string,65 to 90 used for Upper case 
     print("its a CAPITAL Letter")
-elif (ord(chr)>=97 and ord(chr)<=122):
+elif (ord(chr)>=97 and ord(chr)<=122):#ASCII code 97 to 122 used for Small letters
     print ("its a Small charector")
 else:
-    print("errrrr404")
+    print("out of Range")
