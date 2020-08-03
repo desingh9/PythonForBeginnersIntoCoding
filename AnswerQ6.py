3014f8f03862ff57b3c4b9a527958d025ca941de
devendaSingh <desingh319@gmail.com>
2020-08-03 19:03:46 +0530

    Alphabet vowel and constant - Answer

--- AnswerQ6.py
+++ AnswerQ6.py
@@ -1,6 +1,14 @@
 #6.) Write a program to check vowel or consonant.
-charactor=input("please input charactor")
+I=(input("please input letter from Small Alphabet: "))
 
-if (charactor==a,e,i,o,u):
-    print("its a vowel ")
-else()
+if I in ('a' , 'e','i','o','u'):
+    print("[] is a vowel ")
+else:
+    print (" %s is a constant")
+
+#program to check Charactor is Vowel or Constant
+ch=input("please input a Alphabet")
+if (ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U' ):
+    print( "the given alphabet", ch, "is vowel")
+else:
+    print("the given alphabet", ch, "is Constant")
