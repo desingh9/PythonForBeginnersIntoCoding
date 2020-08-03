344299bb96d8a827b1bd67095329afb616b9b274
devendaSingh <desingh319@gmail.com>
2020-08-03 07:30:36 +0530

    Answer 2

--- Answer2_if_elif.py
+++ Answer2_if_elif.py
@@ -1,5 +1,9 @@
 #2.) Take input of age of 3 people by user and determine oldest and youngest among them.
-
-ageOfPeople =[28,24,41]
-print("After Sorting" ,ageOfPeople)
-
+ages=input("enter the age of Person with Space")
+agesList=[]
+agesList=ages.split (" ")
+for ages in agesList:
+    agesList.append (int)(age) #to convert ages value in integer 
+print("Before" ,agesList)
+agesList.sort()
+print("after" ,ageslist)
