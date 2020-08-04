6a8d782cce104e0f482922e73fbd99c8af3cdbfd
devendaSingh <desingh319@gmail.com>
2020-08-05 00:46:39 +0530

    date and month Q

---
+++ Day6_Only_Answers/Answer7_date_month.py
@@ -0,0 +1,4 @@
+#7.) Write a Program to print a number of days in month without using any inbuilt library. 
+#User will enter the month: 7 , output: 31 days .#Take assumption that month 2 that is feB will have 28 only. 
+# as we are not asking year here. so assume the month day to be 28.
+
