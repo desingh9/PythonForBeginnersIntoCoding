2136160ed3a129fc02f3b0152b609a7d864ba4ab
devendaSingh <desingh319@gmail.com>
2020-08-05 00:10:32 +0530

    update

---
+++ Day6_Only_Answers/Answer4_if_else.py
@@ -0,0 +1,17 @@
+#4.) Write a program to check whether triangle is equilateral, scalene or isosceles
+    # Isosceles triangle: In geometry, an isosceles triangle is a triangle that has two sides of equal length.
+    #Equilateral triangle: In geometry, an equilateral triangle is a triangle in which all three sides are equal.
+    #Scalene triangle: A scalene triangle is a triangle that has three unequal sides.
+
+input ("enter the length of triangle sides: ")
+x=int ( input ("x: " ))
+y=int (input ("y :"))
+z=int (input("z :"))
+if x==y==z:
+    print ("Equilateral Triangle")
+elif x==y or y==z or z==x:
+    print('isosceles tiangle')
+else:
+    print ("scalene triangle")
+
+ 
