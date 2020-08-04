7b41f822b421ef3b12249b3d0dbf93c68cf0ad66
devendaSingh <desingh319@gmail.com>
2020-08-04 14:58:41 +0530

    answer6 update

--- AnswerQ6.py
+++ AnswerQ6.py
@@ -2,12 +2,12 @@
 I=(input("please input letter from Small Alphabet: "))
 
 if I in ('a' , 'e','i','o','u'):
-    print("[] is a vowel ")
+    print(","" is a vowel ")
 else:
     print (" %s is a constant")
 
-#program to check Charactor is Vowel or Constant
-ch=input("please input a Alphabet")
+#2 step - program to check Charactor is Vowel or Constant
+ch=input("please input a Alphabet in Small or Capital letter")
 if (ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U' ):
     print( "the given alphabet", ch, "is vowel")
 else:
