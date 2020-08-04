565790874185779fd180eacd474efad259da1112
devendaSingh <desingh319@gmail.com>
2020-08-04 18:09:14 +0530

    file moved to folder

---
+++ Day6_Only_Answers/Answer5_if_else.py
@@ -0,0 +1,9 @@
+#5.) Write a program to check whether a character is an alphabet or not.
+#  whether charcter entered is a-z or A-Z or not ?
+
+chr=input("please Enter Charactor :")
+if  (ord(chr)>=65 and ord(chr) <=90) or (ord(chr) >= 97 and (ord(chr) <=122)): 
+    print (" charactor" , chr, " is a Alphabet")
+else:
+    print ("this is not a Alphabet")
+    
