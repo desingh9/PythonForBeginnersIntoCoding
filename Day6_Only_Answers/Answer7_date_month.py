11f8a81746fa5b2f9f75d31e0f666845ea53b526
devendaSingh <desingh319@gmail.com>
2020-08-07 09:10:20 +0530

    Days in month Logic

--- Day6_Only_Answers/Answer7_date_month.py
+++ Day6_Only_Answers/Answer7_date_month.py
@@ -2,12 +2,16 @@
 #User will enter the month: 7 , output: 31 days .#Take assumption that month 2 that is feB will have 28 only. 
 # as we are not asking year here. so assume the month day to be 28.
 
-month=input("Pleaes enter the month")
-daysofmonths = ("Jan","Mar", "May", "Jul", "Aug", "Oct","Dec")
-def numberOfDays():
-list = [1,3,5,7,8,10,12]
-if (input (list))
-    print ("no of days is 31")
+m=int(input("Pleaes enter the month number"))
+#daysofmonths = ("Jan","Mar", "May", "Jul", "Aug", "Oct","Dec")
 
-elif (int (input 2):
-    print ( "no of days are 28")
+List=[1,3,5,7,8,10,12]
+if m==2:
+    print ("no of days in February 28 or 29")
+elif m in List:
+    print ("no of days are 31 in this month")
+elif m==4 or m==6 or m==9 or m==11:
+    print ("days in month is 30")
+else:
+    print ("invalid input please try again")
+    
