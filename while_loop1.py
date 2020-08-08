a21f2fd7ce843b33743d9ccc3e8982e65a98b72c
devendaSingh <desingh319@gmail.com>
2020-08-09 01:29:29 +0530

    while

---
+++ while_loop1.py
@@ -0,0 +1,12 @@
+#1. **Easiest**    Print one asterisk to the console. 
+#     Example:
+  #  *
+
+num=int(input ("enter the number of rows"))
+row=0
+while   row < num:
+    star=row+1
+    while star>0:
+        print ("*")
+        row=row+1
+        
