3cf06f3e6a4bf8cd786c45074e20a370e1e2e0b3
devendaSingh <desingh319@gmail.com>
2020-08-14 16:13:31 +0530

    update-14-Aug

--- Day4_loops/For_loop_table.py
+++ Day4_loops/For_loop_table.py
@@ -1,4 +1,4 @@
 #Print a table using for loop 
 t=int(input("enter the number : "))
-for table in range (1,10+1):
+for table in range (1,10+1):  # this will print from 1- 10 number as given 
     print (t, "x",table, "=", t * table )
