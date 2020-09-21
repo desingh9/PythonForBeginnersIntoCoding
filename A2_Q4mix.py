244525172c7ca119dfe441617e770df760a2dbf6
devendaSingh <desingh319@gmail.com>
2020-09-21 10:27:47 +0530

    update

--- A2_Q4mix.py
+++ A2_Q4mix.py
@@ -1,8 +1,8 @@
 #2)	Write a program to find the duplicate number on a given integer array? 1,3,3,4 =3 or 1929 =9
-arr= [0,1,2,3,2,4,5,8,8,4]
+arr= [0,1,2,3,2,4,5,5,8,8,4]
 print ("Duplicate element in given arrey: ")
 #Searches for Duplicate elements
 for i in range (0,len (arr)):
-a    for j in range (i+1, len (arr)):
+       for j in range (i+1, len (arr)):
         if (arr[i]== arr[j]):
             print (":" ,arr[j] , end =(" "))
