70c3143255b53308f7497f909ca61c4a0baaed1c
devendaSingh <desingh319@gmail.com>
2020-10-04 16:48:23 +0530

    update

--- A4_Q4.py
+++ A4_Q4.py
@@ -1,1 +1,19 @@
 #Write a program to check if a given string is a palindrome? 
+def isPalidrome(str):
+   # return s== s[::-1]
+   #Run loop from 0 to len/2
+
+    for i in range(0,int (len(str))):
+        if str[i] !=str [len(str)-i-1]:
+            return False
+    return True
+
+
+s="North india"
+ans=isPalidrome(s)
+
+if (ans):
+     print ("Yes")
+
+else:
+     print("no")
